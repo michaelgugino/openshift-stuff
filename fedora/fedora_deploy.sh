d8999d040beb457d4e89537ee7b682350940f0ca
Michael Gugino <mgugino@redhat.com>
2018-01-07 17:49:44 -0500

    add fedora tools

---
+++ fedora/fedora_deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+APB3="`which python3` `which ansible-playbook`"
+$APB3 -i inv-fedora.txt -e @extra_vars.yml ~/git/openshift-ansible/playbooks/prerequisites.yml -vvv
+$APB3 -i inv-fedora.txt -e @extra_vars.yml ~/git/openshift-ansible/playbooks/deploy_cluster.yml -vvv
