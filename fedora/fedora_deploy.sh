e96e348994ee6ff4ab97a4380bdf8bd6506e72e7
Michael Gugino <mgugino@redhat.com>
2018-02-09 13:05:03 -0500

    fix inventories and set -e

--- fedora/fedora_deploy.sh
+++ fedora/fedora_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 APB3="`which python3` `which ansible-playbook`"
 $APB3 -i inv-fedora.txt -e @extra_vars.yml ~/git/openshift-ansible/playbooks/prerequisites.yml -vvv
 $APB3 -i inv-fedora.txt -e @extra_vars.yml ~/git/openshift-ansible/playbooks/deploy_cluster.yml -vvv
